77245919edb7af56ac1deadec6b9df821fd8e268
Luke-544 <lukesamuels422@gmail.com>
2024-03-27 22:01:16 +0300

    Update day12.py

--- day12.py
+++ day12.py
@@ -1,23 +1,23 @@
 #return statement- send python value/objects back to the caller
-#def multiply(number1, number2):
-  #  result = number1 * number2
- #   return result
+def multiply(number1, number2):
+    result = number1 * number2
+    return result
 
-#x = multiply(4,5)
+x = multiply(4,5)
 
-#print(x)
+print(x)
 #or
-#print(multiply(4,5))
+print(multiply(4,5))
 
 #you can reduce the amount of code by:
-#def multiply(number1, number2):
- #   return number1 * number2
+def multiply(number1, number2):
+    return number1 * number2
 
-#x = multiply(4,5)
+x = multiply(4,5)
 
-#print(x)
+print(x)
 #or
-#print(multiply(4,5))
+print(multiply(4,5))
 
 #Keyword Arguments- The order of the arguments does not matter
 
